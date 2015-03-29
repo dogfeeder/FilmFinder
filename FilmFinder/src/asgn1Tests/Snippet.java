42e953a19e210f28831fe68d29d081d65ada785b
Patrick Freeman <trick05@bigpond.net.au>
2015-03-29 15:23:08 +1000

    First commit

---
+++ FilmFinder/src/asgn1Tests/Snippet.java
@@ -0,0 +1,25 @@
+package asgn1Tests;
+
+//The test for the addRecord call after the ArrayList has been sorted. Please listen to the 
+//podcast for details. 
+//Note that you will have to declare appropriate variables to make this work, but these will 
+//be required for most of your tests. 
+
+//This test should end up in the class RecordCollectionTests
+
+public class Snippet {
+	  /** @author samuelbr
+	     * Test method for {@link asgn1Index.RecordCollection#isSorted()}.
+	     * Check that sorted flag is unset after more records are added after call to <code>sortCollection</code>.
+	     * @throws IndexException 
+	     */
+	    @Test
+	    public void collectionNotSortedAfterSortingThenAdding() throws IndexException {
+	        unsortedRecordCollection.sortCollection();
+	        assertTrue(unsortedRecordCollection.isSorted());
+	        unsortedRecordCollection.addRecord(moon);
+	        assertFalse(unsortedRecordCollection.isSorted());
+	    }
+	
+}
+
